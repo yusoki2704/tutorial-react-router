62ad54536cf50220877b240e0b75fdcc0b23986d
yusoki2704 <71821574+yusoki2704@users.noreply.github.com>
2021-05-08 10:08:54 +0900

    fix react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+
+import { Home } from "./Home";
+import { Page1 } from "./Page1";
+import { Page2 } from "./Page2";
 import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/page1">
+          <Page1 />
+        </Route>
+      </Switch>
+      <Route path="/page2">
+        <Page2 />
+      </Route>
+    </BrowserRouter>
   );
 }
