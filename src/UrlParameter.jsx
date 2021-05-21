b36e0d32afd90c459bbba507e87cf0dae300504f
yusoki2704 <71821574+yusoki2704@users.noreply.github.com>
2021-05-22 07:18:38 +0900

    add Page1Routes.jsx

---
+++ src/UrlParameter.jsx
@@ -0,0 +1,7 @@
+export const UrlParameter = () => {
+  return (
+    <div>
+      <h1>UrlParameterページです</h1>
+    </div>
+  );
+};
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,10 +1,8 @@
 import { Switch, Route } from "react-router-dom";
 
 import { Home } from "../Home";
-import { Page1 } from "../Page1";
-import { Page1DetailA } from "../Page1DetailA";
-import { Page1DetailB } from "../Page1DetailB";
 import { Page2 } from "../Page2";
+import { page1Routes } from "./Page1Routes";
 
 export const Router = () => {
   return (
@@ -16,15 +14,15 @@ export const Router = () => {
         path="/page1"
         render={({ match: { url } }) => (
           <Switch>
-            <Route exact path={url}>
-              <Page1 />
-            </Route>
-            <Route path={`${url}/detailA`}>
-              <Page1DetailA />
-            </Route>
-            <Route path={`${url}/detailB`}>
-              <Page1DetailB />
-            </Route>
+            {page1Routes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
           </Switch>
         )}
       ></Route>
