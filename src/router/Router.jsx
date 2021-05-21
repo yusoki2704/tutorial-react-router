97f97e56804ffb65ae5790f04671708b62570981
yusoki2704 <71821574+yusoki2704@users.noreply.github.com>
2021-05-22 07:28:30 +0900

    add UrlParamerer.jsx

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,8 +1,8 @@
 import { Switch, Route } from "react-router-dom";
 
 import { Home } from "../Home";
-import { Page2 } from "../Page2";
 import { page1Routes } from "./Page1Routes";
+import { page2Routes } from "./Page2Routes";
 
 export const Router = () => {
   return (
@@ -26,9 +26,22 @@ export const Router = () => {
           </Switch>
         )}
       ></Route>
-      <Route path="/page2">
-        <Page2 />
-      </Route>
+      <Route
+        path="/page2"
+        render={({ match: { url } }) => (
+          <Switch>
+            {page2Routes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
+        )}
+      ></Route>
     </Switch>
   );
 };
